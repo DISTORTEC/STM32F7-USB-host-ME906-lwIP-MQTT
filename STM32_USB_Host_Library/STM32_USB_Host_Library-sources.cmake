baaa5ad3ca17e0c2c18fb7796e058f53f2bd3d57
Kamil Szczygiel <distortec@distortec.com>
2020-04-18 17:30:52 +0200

    Update STM32F7xx_HAL_Driver and ...-integration submodules

--- STM32_USB_Host_Library/STM32_USB_Host_Library-sources.cmake
+++ STM32_USB_Host_Library/STM32_USB_Host_Library-sources.cmake
@@ -1,7 +1,7 @@
 #
 # file: STM32_USB_Host_Library-sources.cmake
 #
-# author: Copyright (C) 2019 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+# author: Copyright (C) 2019-2020 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
 #
 # This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
 # distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -20,4 +20,4 @@ target_include_directories(STM32_USB_Host_Library PUBLIC
 		${CMAKE_CURRENT_LIST_DIR}/Core/Inc)
 target_link_libraries(STM32_USB_Host_Library PUBLIC
 		CMSIS-RTOS2
-		STM32F7xx_HAL_Driver::STM32F7xx_HAL_Driver)
+		STM32F7xx_HAL_Driver)
