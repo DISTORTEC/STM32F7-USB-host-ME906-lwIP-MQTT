d0aff9e96c8d3e9e54e109a85e329ce5ded223f5
Kamil Szczygiel <distortec@distortec.com>
2020-04-18 23:08:50 +0200

    Some AT commands report an error with "+CME ERROR:..."

--- PpposManager.cpp
+++ PpposManager.cpp
@@ -2,7 +2,7 @@
  * \file
  * \brief PpposManager class implementation
  *
- * \author Copyright (C) 2019 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ * \author Copyright (C) 2019-2020 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
  *
  * \par License
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
@@ -71,6 +71,7 @@ void PpposManager::chatScriptPhase() const
 
 	static const char* const abortStrings[]
 	{
+			"+CME ERROR:",
 			"ERROR",
 			"NO CARRIER"
 	};
